d0d3138ea9a0d557ceb8570c5c4e1b69b49c27b3
Filip Szydłak <266912@student.pwr.edu.pl>
2023-05-20 22:53:27 +0200

    product-list component updated, crossorigin policy add

--- backend/spring-boot-ecommerce/src/main/java/pl/filipszydlak/ecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/pl/filipszydlak/ecommerce/dao/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package pl.filipszydlak.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.filipszydlak.ecommerce.entity.ProductCategory;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/spring-boot-ecommerce/src/main/java/pl/filipszydlak/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/pl/filipszydlak/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,8 @@
 package pl.filipszydlak.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.filipszydlak.ecommerce.entity.Product;
-
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
