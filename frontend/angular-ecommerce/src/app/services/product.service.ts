0bbe31398085b5a33b89f228c2137234bfd126f4
Filip Szydłak <266912@student.pwr.edu.pl>
2023-05-20 22:39:06 +0200

    ProductService update

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {Product} from "../common/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+private baseUrl ='http://localhost:8080/api/products'
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  // map the JSON data from Spring Data REST to Product array
+  getProductList(): Observable<Product[]>{
+  return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    map(response => response._embedded.products)
+  )
+  }
+
+}
+interface GetResponse{
+  _embedded:{
+    products: Product[];
+  }
 }
