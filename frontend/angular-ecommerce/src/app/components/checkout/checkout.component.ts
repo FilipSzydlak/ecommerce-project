4baf334f7f5301593e5120503ffab65814a66611
Filip Szydłak <266912@student.pwr.edu.pl>
2023-06-04 16:57:01 +0200

    shopFormService injected

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {CartService} from "../../services/cart.service";
+import {ShopFormService} from "../../services/shop-form.service";
 
 @Component({
   selector: 'app-checkout',
@@ -14,7 +15,10 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder, private cartService: CartService) {
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder, private cartService: CartService, private shopFormService: ShopFormService) {
   }
 
   ngOnInit() {
@@ -48,6 +52,26 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+
+    // populate credit card months
+
+    const startMonth:number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data =>{
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+
+    // populate credit card months
+    this.shopFormService.getCreditCardYears().subscribe(
+      data =>{
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    )
 
     this.cartService.totalPrice.subscribe(
       data => this.totalPrice = data
